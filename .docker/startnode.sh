dabcc57d1722c8b81991b56d35fa9892a1650195
Jonathan Logan <JonathanLogan@users.noreply.github.com>
2021-12-20 14:12:12 +0000

    Updated docker

--- .docker/startnode.sh
+++ .docker/startnode.sh
@@ -10,7 +10,21 @@ fi
 
 if [ ! -d /near/${network} ]; then
 	mkdir /near/${network}
-	/usr/local/bin/borealis-indexer --home-dir /near/${network} init --chain-id ${network} --boot-nodes "" --download-genesis --download-genesis-url "" --download-config --download-config-url "https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/${network}/config.json"
 fi
 
-/usr/local/bin/borealis-indexer --home-dir /near/testnet run --creds-path "/near/testnet/nats.creds" --nats-server nats://westcoast.nats.backend.aurora.dev:4222 --subject "BlockIndex_StreamerMessages" --msg-format JSON
+if [ ! -f /near/${network}/config.json ]; then
+	curl -o /near/${network}/config.json https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/testnet/config.json
+fi
+
+if [ ! -f /near/${network}/genesis.json ]; then
+	curl -o /near/${network}/genesis.json https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/testnet/genesis.json
+fi
+
+if [ ! -f /near/server ]; then
+	echo "nats://westcoast.nats.backend.aurora.dev:4222" > /near/server
+fi
+if [ ! -f /near/subject ]; then
+	echo "BlockIndex_StreamerMessages" > /near/subject
+fi
+
+/usr/local/bin/borealis-indexer --home-dir /near/testnet run --creds-path "/near/nats.creds" --nats-server "$(cat /near/server)" --subject "$(cat /near/subject)" --msg-format CBOR
