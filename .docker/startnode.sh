f5317bb06f664e0b0230a15681d7040577bba64b
Jonathan Logan <JonathanLogan@users.noreply.github.com>
2021-12-21 15:42:51 +0000

    Update startnode.sh

--- .docker/startnode.sh
+++ .docker/startnode.sh
@@ -20,6 +20,14 @@ if [ ! -f /near/${network}/genesis.json ]; then
 	curl -o /near/${network}/genesis.json https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/testnet/genesis.json
 fi
 
+if [ ! -f /near/${network}/node_key.json ]; then
+	/usr/local/bin/nearkey "node%.${namePostfix}" > /near/${network}/node_key.json
+fi
+
+if [ ! -f /near/${network}/validator_key.json ]; then
+	/usr/local/bin/nearkey "validator%.${namePostfix}" > /near/${network}/validator_key.json
+fi
+
 if [ ! -f /near/server ]; then
 	echo "nats://westcoast.nats.backend.aurora.dev:4222" > /near/server
 fi
@@ -27,4 +35,4 @@ if [ ! -f /near/subject ]; then
 	echo "BlockIndex_StreamerMessages" > /near/subject
 fi
 
-/usr/local/bin/borealis-indexer --home-dir /near/testnet run --creds-path "/near/nats.creds" --nats-server "$(cat /near/server)" --subject "$(cat /near/subject)" --msg-format CBOR
+/usr/local/bin/borealis-indexer --home-dir /near/${network} run --creds-path "/near/nats.creds" --nats-server "$(cat /near/server)" --subject "$(cat /near/subject)" --msg-format CBOR
