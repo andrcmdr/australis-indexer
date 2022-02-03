22fb579a4389ac7da3cfdde007f217bf1108b3cc
Andrew Bednoff <andrew.bednoff@aurora.dev>
2022-02-03 11:35:37 +0600

    Added Indexer's `check` subcommand (for NATS connection check) to starter shell scripts.

--- .bootstrap/localnet/indexer.run.sh
+++ .bootstrap/localnet/indexer.run.sh
@@ -9,7 +9,8 @@ set -f
 
 echo -e "\n";
 
-time /bin/time -v env RUST_BACKTRACE=full ./borealis-indexer --home-dir ./.borealis-indexer/localnet/ run --root-cert-path ./.nats/seed/root-ca.crt --creds-path ./.nats/seed/nats.creds --nats-server "tls://westcoast.nats.backend.aurora.dev:4222,tls://eastcoast.nats.backend.aurora.dev:4222" --subject "BlockIndex_StreamerMessages_localnet" --msg-format "CBOR" >> ./log/borealis-indexer.debug.log 2>&1 & disown;
+# time /bin/time -v env RUST_BACKTRACE=full ./borealis-indexer --home-dir ./.borealis-indexer/localnet/ check --root-cert-path ./.nats/seed/root-ca.crt --creds-path ./.nats/seed/nats.creds --nats-server "tls://westcoast.nats.backend.aurora.dev:4222,tls://eastcoast.nats.backend.aurora.dev:4222" --subject "BlockIndex_StreamerMessages_localnet" --msg-format "CBOR" --sync-mode "LatestSynced" --await-synced "WaitForFullSync" >> ./log/borealis-indexer.debug.log 2>&1 & disown;
+time /bin/time -v env RUST_BACKTRACE=full ./borealis-indexer --home-dir ./.borealis-indexer/localnet/ run --root-cert-path ./.nats/seed/root-ca.crt --creds-path ./.nats/seed/nats.creds --nats-server "tls://westcoast.nats.backend.aurora.dev:4222,tls://eastcoast.nats.backend.aurora.dev:4222" --subject "BlockIndex_StreamerMessages_localnet" --msg-format "CBOR" --sync-mode "LatestSynced" --await-synced "WaitForFullSync" >> ./log/borealis-indexer.debug.log 2>&1 & disown;
 
 echo -e "\n";
 
