945c71063bd29e1aeb9bb04ae040be60d456a1fe
Andrew Bednoff <andrew.bednoff@aurora.dev>
2022-03-07 17:37:59 +0600

    [WIP: Connection Pool] Run-time tuning. Tracking most actual workable connection. Using most actual connection in streamer message handler via receiving channel.

--- borealis-indexer/src/main.rs
+++ borealis-indexer/src/main.rs
@@ -9,13 +9,14 @@ use serde_cbor as cbor;
 use serde_json;
 use tokio::sync::{mpsc, broadcast};
 use tokio::runtime;
+use core::sync::atomic::{AtomicUsize, Ordering};
 use tracing::info;
 
 pub mod configs;
 
 async fn message_producer(
     mut events_stream: mpsc::Receiver<near_indexer::StreamerMessage>,
-    nats_connection: nats::Connection,
+    mut actual_connection_rx: broadcast::Receiver<NATSConnection>,
     subject: String,
     msg_format: MsgFormat,
     verbosity_level: Option<VerbosityLevel>,
@@ -262,6 +263,13 @@ async fn message_producer(
             target: "borealis_indexer",
             "Message producer loop executed: message received\n"
         );
+
+        let nats_connection = {
+            match actual_connection_rx.recv().await {
+                Ok(nats_connection) => Ok(nats_connection),
+                Err(_error) => actual_connection_rx.recv().await,
+            }
+        }.unwrap().connection.unwrap();
 
         // Stream message to NATS
         match msg_format {
@@ -635,15 +643,54 @@ fn main() -> Result<(), Error> {
         .home_dir
         .unwrap_or(std::path::PathBuf::from("./.borealis-indexer"));
 
-    let events_processing = runtime::Builder::new_multi_thread().enable_all().thread_name("connection-events-processing").build()?;
-    let messages_processing = runtime::Builder::new_multi_thread().enable_all().thread_name("streamer-messages-processing").build()?;
+    let events_processing = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_name_fn( || {
+            static THREAD_ID: AtomicUsize = AtomicUsize::new(0);
+            let thread_id = THREAD_ID.fetch_add(1, Ordering::SeqCst);
+            format!("connection-events-processing-{}", thread_id)
+         })
+        .on_thread_start( || {
+            info!(target: "borealis_indexer", "NATS connection events processing runtime: thread starting");
+        })
+        .on_thread_stop( || {
+            info!(target: "borealis_indexer", "NATS connection events processing runtime: thread stopping");
+        })
+        .on_thread_park( || {
+            info!(target: "borealis_indexer", "NATS connection events processing runtime: thread parking and going idle");
+        })
+        .on_thread_unpark( || {
+            info!(target: "borealis_indexer", "NATS connection events processing runtime: thread unparked and starts executing tasks");
+        })
+        .build()?;
+
+    let messages_processing = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_name_fn( || {
+            static THREAD_ID: AtomicUsize = AtomicUsize::new(0);
+            let thread_id = THREAD_ID.fetch_add(1, Ordering::SeqCst);
+            format!("streamer-messages-processing-{}", thread_id)
+         })
+        .on_thread_start( || {
+            info!(target: "borealis_indexer", "Streamer Messages processing runtime: thread starting");
+        })
+        .on_thread_stop( || {
+            info!(target: "borealis_indexer", "Streamer Messages processing runtime: thread stopping");
+        })
+        .on_thread_park( || {
+            info!(target: "borealis_indexer", "Streamer Messages processing runtime: thread parking and going idle");
+        })
+        .on_thread_unpark( || {
+            info!(target: "borealis_indexer", "Streamer Messages processing runtime: thread unparked and starts executing tasks");
+        })
+        .build()?;
 
     let (connection_event_tx, mut connection_event_rx) = mpsc::channel::<ConnectionEvent>(1);
     let (actual_connection_tx, mut actual_connection_rx) = broadcast::channel::<NATSConnection>(1);
 
     let connection_event_sender = connection_event_tx.clone();
     let actual_connection_sender = actual_connection_tx.clone();
-    let mut actual_connection_receiver = actual_connection_tx.subscribe();
+    let actual_connection_receiver = actual_connection_tx.subscribe();
 
     // let events_processing_handle = events_processing.handle();
     // events_processing_handle.enter();
@@ -655,18 +702,33 @@ fn main() -> Result<(), Error> {
                 match event {
                     ConnectionEvent::ConnectionReestablished => {
                         info!(target: "borealis_indexer", "connection has been reestablished, thus checking connection is active...");
-                        let nats_connection = actual_connection_receiver.recv().await.unwrap();
-                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_sender.clone(), actual_connection_sender.clone()).unwrap();
+                        let nats_connection = {
+                            match actual_connection_rx.recv().await {
+                                Ok(nats_connection) => Ok(nats_connection),
+                                Err(_error) => actual_connection_rx.recv().await,
+                            }
+                        }.unwrap();
+                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_tx.clone(), actual_connection_tx.clone()).unwrap();
                     },
                     ConnectionEvent::ConnectionLost => {
                         info!(target: "borealis_indexer", "connection has been lost, thus retrieving connection...");
-                        let nats_connection = actual_connection_receiver.recv().await.unwrap();
-                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_sender.clone(), actual_connection_sender.clone()).unwrap();
+                        let nats_connection = {
+                            match actual_connection_rx.recv().await {
+                                Ok(nats_connection) => Ok(nats_connection),
+                                Err(_error) => actual_connection_rx.recv().await,
+                            }
+                        }.unwrap();
+                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_tx.clone(), actual_connection_tx.clone()).unwrap();
                     },
                     ConnectionEvent::ConnectionClosed => {
                         info!(target: "borealis_indexer", "connection has been closed, thus retrieving connection...");
-                        let nats_connection = actual_connection_receiver.recv().await.unwrap();
-                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_sender.clone(), actual_connection_sender.clone()).unwrap();
+                        let nats_connection = {
+                            match actual_connection_rx.recv().await {
+                                Ok(nats_connection) => Ok(nats_connection),
+                                Err(_error) => actual_connection_rx.recv().await,
+                            }
+                        }.unwrap();
+                        let _nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_tx.clone(), actual_connection_tx.clone()).unwrap();
                     },
                 }
             }
@@ -675,8 +737,8 @@ fn main() -> Result<(), Error> {
 
     match opts.subcmd {
         SubCommand::Check(run_args) => {
-            let nats_connection = NATSConnection::connect(run_args.to_owned(), connection_event_tx.clone(),  actual_connection_tx.clone()).unwrap();
-            let nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_tx.clone(), actual_connection_tx.clone()).unwrap();
+            let nats_connection = NATSConnection::connect(run_args.to_owned(), connection_event_sender.clone(),  actual_connection_sender.clone()).unwrap();
+            let nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_sender.clone(), actual_connection_sender.clone()).unwrap();
             nats_connection_actual.nats_check_connection();
         }
         SubCommand::Init(config_args) => {
@@ -705,8 +767,8 @@ fn main() -> Result<(), Error> {
                 },
             };
 
-            let nats_connection = NATSConnection::connect(run_args.to_owned(), connection_event_tx.clone(),  actual_connection_tx.clone()).unwrap();
-            let nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_tx.clone(), actual_connection_tx.clone()).unwrap();
+            let nats_connection = NATSConnection::connect(run_args.to_owned(), connection_event_sender.clone(),  actual_connection_sender.clone()).unwrap();
+            let nats_connection_actual = nats_connection.try_connect(run_args.to_owned(), connection_event_sender.clone(), actual_connection_sender.clone()).unwrap();
             nats_connection_actual.to_owned().nats_check_connection();
 
             messages_processing.spawn(async move {
@@ -717,7 +779,7 @@ fn main() -> Result<(), Error> {
 
                 tokio::spawn(async move { message_producer(
                     events_stream,
-                    actual_connection_rx.recv().await.unwrap().connection.unwrap(),
+                    actual_connection_receiver,
                     run_args.subject,
                     run_args.msg_format,
                     opts.verbose,
