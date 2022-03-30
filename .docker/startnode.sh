bd967d13902295c4ea31cb371e8728d9bee7f005
Jonathan Logan <jonathan.logan@aurora.dev>
2022-03-30 16:29:23 +0200

    new docker

--- .docker/startnode.sh
+++ .docker/startnode.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-
-network="testnet"
-
-if $(is-mainnet); then
-    network="mainnet"
-fi
-
-if [ ! -d /borealis-indexer/${network} ]; then
-    mkdir /borealis-indexer/${network}
-    /usr/local/bin/borealis-indexer --home-dir /borealis-indexer/${network} init --chain-id ${network} --download-genesis --download-config
-fi
-
-/usr/local/bin/borealis-indexer --home-dir /borealis-indexer/${network} run --root-cert-path /borealis-indexer/${network}/root-ca.crt --creds-path /borealis-indexer/${network}/nats.creds --nats-server "tls://eastcoast.nats.backend.aurora.dev:4222,tls://westcoast.nats.backend.aurora.dev:4222" --subject "BlockIndex_StreamerMessages_${network}" --msg-format "CBOR"
+/usr/local/bin/borealis-indexer --home-dir /home/near run --root-cert-path /home/near/ca.crt --creds-path /home/near/nats.creds --nats-server nats://genesis.nats.backend.aurora.dev:4222 --msg-format CBOR  --subject BlockIndex_StreamerMessages_testnet_CBOR 2>&1 | tee -a /home/near/indexer.log
