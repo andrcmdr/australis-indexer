82c3314e0468a1d199811a5fc19cb4f284758443
Andrew Bednoff <andrew.bednoff@aurora.dev>
2022-04-13 15:37:59 +0600

    Updating scripts for downloading node state.

--- .dl/localnet/wget.sh
+++ .dl/localnet/wget.sh
@@ -7,5 +7,5 @@ shopt -s extquote
 
 set -f
 
-wget --verbose --report-speed=bits --append-output=wget.log --trust-server-names --content-disposition --tries=30 --continue --progress=bar --show-progress --timestamping --server-response --dns-timeout=60 --connect-timeout=60 --read-timeout=60 --waitretry=60 --prefer-family=IPv4 --retry-connrefused --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0' --referer= --recursive --level=30 --no-parent --no-directories --no-host-directories --directory-prefix=./ --input-file='./download.list' "${@}" ;
+wget --verbose --report-speed=bits --append-output=wget.log --trust-server-names --content-disposition --tries=100 --continue --progress=bar --show-progress --timestamping --server-response --dns-timeout=60 --connect-timeout=60 --read-timeout=60 --waitretry=60 --prefer-family=IPv4 --retry-connrefused --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0' --referer= --recursive --level=30 --no-parent --no-directories --no-host-directories --directory-prefix=./ --input-file='./download.list' "${@}" ;
 
